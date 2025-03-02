d2500f021274a3b55c78af44f2458d1c44bfedd1
Anuj das <anujd339@gmail.com>
2025-03-02 15:13:13 +0530

    development

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 document.getElementById("toggle-btn").addEventListener("click", function () {
     let sidebar = document.getElementById("sidebar");
-    let menubar = document.getElementById("menubar");
     let toggleIcon = document.getElementById("toggle-icon");
     let companyName = document.getElementById("company-name");
     sidebar.classList.toggle("collapsed");
-    menubar.classList.toggle("collapsed");
     toggleIcon.classList.toggle("rotated");
     companyName.style.display = sidebar.classList.contains("collapsed") ? "none" : "block";
 });
